29c7b59cbaffe52fddfcbb46a42852b49fd7982e
Saurabh2619 <sharmasaurabh2606@gmail.com>
2026-06-08 13:25:42 +0530

    Restore test-vpulse page

---
+++ app/test-vpulse/page.tsx
@@ -0,0 +1,9 @@
+import TestVPulseForm from '../components/TestVPulseForm';
+
+export default function TestVPulsePage() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-12">
+      <TestVPulseForm />
+    </div>
+  );
+}
