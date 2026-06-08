aebed2875c529e81cbae4c46ac0a6bd81233d4ea
Saurabh2619 <sharmasaurabh2606@gmail.com>
2026-06-08 14:14:37 +0530

    Clean up test vpulse pages

--- app/test-vpulse/page.tsx
+++
@@ -1,9 +0,0 @@
-import TestVPulseForm from '../components/TestVPulseForm';
-
-export default function TestVPulsePage() {
-  return (
-    <div className="min-h-screen bg-gray-100 py-12">
-      <TestVPulseForm />
-    </div>
-  );
-}
