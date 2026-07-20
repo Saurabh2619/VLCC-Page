790406b1db5b5d1c1450e3491dbab28d292bdfa3
Saurabh2619 <sharmasaurabh2606@gmail.com>
2026-07-20 17:13:35 +0530

    meta property tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,18 @@ export const metadata: Metadata = {
   title: "Beauty, Makeup, Hair & Wellness Academy | VLCC Education",
   description: "India's leading beauty academy offering makeup, hair, skincare, nail, nutrition & wellness courses with certification and placement assistance.",
   keywords: "VLCC Institute, Beauty & Makeup Courses, Cosmetology Course, Nutrition Course, Wellness Training, Beauty Academy, Makeup Training",
+  openGraph: {
+    title: "Master Makeup & Beauty Skills at VLCC School of Beauty",
+    siteName: "VLCC Education",
+    url: "https://www.vlcceducation.com/",
+    description: "Learn. Create. Shine. Join VLCC School of Beauty and master the skills of Makeup, Hair, Skin Care, Nail Artistry, Laser Aesthetics, and Nutrition with expert guidance and practical training.",
+    type: "website",
+    images: [
+      {
+        url: "",
+      }
+    ]
+  },
   icons: {
     icon: "/vlcc-logo.png"
   }
